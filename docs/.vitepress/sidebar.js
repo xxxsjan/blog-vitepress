d94d4c2c798ca50e49549a03010dff2c71a3266b
xxxsjan <626653354@qq.com>
2023-04-05 19:56:29 +0800

    移除自定义

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,77 +1,23 @@
-const fg = require('fast-glob');
-const path = require('path');
-import { useLocalDoc } from './custom';
-
-const docsPath = useLocalDoc ? process.cwd() : path.resolve(process.cwd(), '../web-note');
-console.log(docsPath);
-// console.log(
-//   fg.sync('**', {
-//     onlyFiles: false,
-//     cwd: docsPath,
-//     deep: 1,
-//   })
-// );
-
-function getPath(p) {
-  return path.join(docsPath, './docs', p);
-}
-function getDirs(p) {
-  return fg.sync('**', {
-    onlyFiles: false,
-    cwd: getPath(p),
-    deep: 1,
-    ignore: ['*.md'],
-  });
-}
-function getMdFiles(p) {
-  return fg.sync('**', {
-    onlyFiles: true,
-    cwd: getPath(p),
-    deep: 1,
-    ignore: ['index.md'],
-  });
-}
-/**
- *
- * @param {String} dirPath docs文件所在目录下的某个文件夹名
- * @returns
- */
-function genSideBar(dirPath) {
-  // dirPath 文件夹下的所有文件夹
-  const dirs = getDirs(`${dirPath}`);
-  const mdFiles = getMdFiles(`${dirPath}`);
-  let res = [
-    // 有文件夹
-    ...dirs.map((dir) => {
-      let obj = {
-        text: dir,
-        items: genSideBar(`${dirPath}/${dir}`),
-      };
-      if (obj.items.length === 0) {
-        delete obj.items;
-        obj.link = `/${dirPath}/${dir}/`;
-      }
-      return obj;
-    }),
-    // 有md文件
-    ...mdFiles.map((file) => {
-      const text = file.replace('.md', '');
-      return {
-        text: text,
-        link: `/${dirPath}/${text}`,
-      };
-    }),
-  ];
-  return res;
-}
-const webnote = genSideBar('webnote');
-console.log(JSON.stringify(webnote));
-
 export default {
-  '/webnote/': [
+  "/webnote/": [
     {
-      text: 'webnote',
-      items: webnote,
+      text: "webnote",
+      items: [
+        {
+          text: "one",
+          link: "/webnote/one",
+        },
+      ],
+    },
+  ],
+  "/guide/": [
+    {
+      text: "Guide",
+      items: [
+        { text: "Index", link: "/guide/" }, // /guide/index.md
+        { text: "One", link: "/guide/one" }, // /guide/one.md
+        { text: "Two", link: "/guide/two" }, // /guide/two.md
+      ],
     },
   ],
 };
